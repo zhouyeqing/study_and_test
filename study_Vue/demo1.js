51fb0787b4c3951a34e42d20f3d9bfca41ece0c1
zhouyeqing <649951080@qq.com>
2018-10-09 20:08:55 +0800

    临时

--- study_Vue/demo1.js
+++ study_Vue/demo1.js
@@ -1,84 +1,96 @@
-Vue.component('comp1',{
-    props: ['prop1'],
-    template: `<li>{{prop1.name}}</li>`
-});
-Vue.component('button-counter', {
-    data: function () {
-        return {
-            count: 0
-        }
+let app = new Vue({
+    el: '#app',
+    data: {
+        message1: '第一个程序',
+        message2: {
+            'name': '周叶青',
+            'sex': '男'
+        },
+        message3: ['one','two','tree'],
+        "result": [
+            {
+                "id":"242",
+                "catalog":"中国文学"
+            },
+            {
+                "id":"252",
+                "catalog":"人物传记"
+            },
+            {
+                "id":"244",
+                "catalog":"儿童文学"
+            },
+            {
+                "id":"248",
+                "catalog":"历史"
+            },
+            {
+                "id":"257",
+                "catalog":"哲学"
+            },
+            {
+                "id":"243",
+                "catalog":"外国文学"
+            },
+            {
+                "id":"247",
+                "catalog":"小说"
+            },
+            {
+                "id":"251",
+                "catalog":"心灵鸡汤"
+            },
+            {
+                "id":"253",
+                "catalog":"心理学"
+            },
+            {
+                "id":"250",
+                "catalog":"成功励志"
+            },
+            {
+                "id":"249",
+                "catalog":"教育"
+            },
+            {
+                "id":"245",
+                "catalog":"散文"
+            },
+            {
+                "id":"256",
+                "catalog":"理财"
+            },
+            {
+                "id":"254",
+                "catalog":"管理"
+            },
+            {
+                "id":"246",
+                "catalog":"经典名著"
+            },
+            {
+                "id":"255",
+                "catalog":"经济"
+            },
+            {
+                "id":"258",
+                "catalog":"计算机"
+            }
+        ],
+        message4: true,
+        isInfo: true,
+        abc: '123456'
     },
-    template: `<div><button v-on:click="count++">You clicked me {{ count }} times.</button></div>`
+    methods: {
+        show_hide: function () {
+            if (this.message4) {
+                this.message4 = false;
+            } else {
+                this.message4 =true;
+            }
+        },
+        consol: function () {
+            console.log('弹出');
+        }
+    }
 });
-window.onload = function () {
-    let div1 = new Vue({
-        el: "#div1",
-        data: {
-            styleInput: {
-                color: "red"
-            },
-            key: true,
-            message1: "我是zhouyeqing",
-            seen: true,
-            message2: "点击显示消失",
-            text: [
-                {title: "苹果"},
-                {title: "橘子"},
-                {title: "香蕉"},
-                {title: "芒果"},
-                {title: "梨子"}
-            ],
-            message3: [
-                {id: 0,name: "苹果1",seen:true},
-                {id: 1,name: "橘子1",seen:true},
-                {id: 2,name: "香蕉1",seen:true},
-                {id: 3,name: "芒果1",seen:true},
-                {id: 4,name: "梨子1",seen:true}
-            ]
-        },
-        watch: {
-            message1: function () {
-                console.log(this.message1);
-            }
-        },
-        methods: {
-            inputC: function () {
-                this.seen ? this.seen = false : this.seen = true;
-                this.styleInput.color == "red" ? this.styleInput.color = "blue" : this.styleInput.color = "red";
-                this.key ? this.key = false : this.key = true;
-                this.text.push(this.text[Math.floor(Math.random()*5)]);
-            },
-            input1: function () {
-                this.message3[0].seen ? this.message3[0].seen = false : this.message3[0].seen = true;
-            },
-            input2: function () {
-                this.message3[1].seen ? this.message3[1].seen = false : this.message3[1].seen = true;
-            },
-            input3: function () {
-                this.message3[2].seen ? this.message3[2].seen = false : this.message3[2].seen = true;
-            },
-            input4: function () {
-                this.message3[3].seen ? this.message3[3].seen = false : this.message3[3].seen = true;
-            },
-            input5: function () {
-                this.message3[4].seen ? this.message3[4].seen = false : this.message3[4].seen = true;
-            }
-        }
-    });
-    new Vue({
-        el: '#demo',
-        data: {
-            show: true
-        }
-    });
-    new Vue({
-        el: '#example-2',
-        data: {
-            show: true
-        }
-    });
-
-
-
-
-};
